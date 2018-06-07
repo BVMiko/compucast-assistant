3334275156fa62a5385e958d000fd65e96a23c92
brian <brian@diglett>
2018-06-07 11:31:48 -0500

    Migrate from querystring parameters to HTTP headers for all domain toggles, and fix GA debug toggle

--- background.js
+++ background.js
@@ -1,117 +1,107 @@
-function explodeQueryString(qs) {
-	var ret = [];
-	if (qs != "") {
-		var vars = qs.substring(1).split("&");
-		for (var i = 0; i < vars.length; i++) {
-			var [key, val] = vars[i].split('=', 2);
-			key = decodeURIComponent(key);
-			val = val === undefined ? undefined : decodeURIComponent(val);
-			if (key.substr(-2) == "[]") {
-				key = key.substr(0, key.length - 2);
-				if (!Array.isArray(ret[key])) ret[key] = [];
-				ret[key].push(val);
-			} else {
-				ret[key] = val;
-			}
-		}
-	}
-	// console.log('original qs:',  qs);
-	// console.log('exploded qs:',  ret);
-	return ret;
-}
-
-function implodeQueryString(bits) {
-	var ret = Object.entries(bits).map(function(pair) {
-		const [key, val] = pair;
-		if (Array.isArray(val)) {
-			var subret = [];
-			val.forEach(function (subval) { subret.push(encodeURIComponent(key + "[]") + "=" + encodeURIComponent(subval)); });
-			return subret.join("&");
-		} else if (val === undefined) {
-			return encodeURIComponent(key);
-		} else {
-			return encodeURIComponent(key) + "=" + encodeURIComponent(val);
-		}
-	}).join("&");
-	// console.log('imploded qs:',  ret.length ? '?'+ret : '');
-	return ret.length ? '?'+ret : '';
-}
-
-var domainMods = [];
-chrome.storage.local.get(["domainMods"], function(items){
-	if ("domainMods" in items) {
-		domainMods = items["domainMods"];
-	}
+var domains = {}, ga_debug = false;
+chrome.storage.local.get({domains:{}, ga_debug:false}, function(items) {
+	domains = items.domains;
+	ga_debug = items.ga_debug;
 });
 
+// For ga_debug (per-browser)
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	var parser = document.createElement("a");
-	parser.href = details.url;
-
-	// Special stuff for Google Analytics
-	if (["www.google-analytics.com", "ssl.google-analytics.com", "stats.g.doubleclick.net", "www.google-analytics.com"].includes(parser.hostname)) {
-		var initparser = document.createElement("a");
-		initparser.href = details.initiator;
-		currMods = domainMods[initparser.hostname];
-		if ("ga_debug" in currMods && currMods["ga_debug"] != undefined) {
-			var newpathname = parser.pathname;
-			if (parser.hostname == "www.google-analytics.com" && parser.pathname == "/ga.js") {
-				newpathname = "/u/ga_debug.js";
-			} else if (parser.hostname == "ssl.google-analytics.com" && parser.pathname == "/ga.js") {
-				newpathname = "/u/ga_debug.js";
-			} else if (parser.hostname == "stats.g.doubleclick.net" && parser.pathname == "/dc.js") {
-				newpathname = "/dc_debug.js";
-			} else if (parser.hostname == "www.google-analytics.com" && parser.pathname == "/analytics.js") {
-				newpathname = "/analytics_debug.js";
-			}
-			if (newpathname != parser.pathname) {
-				parser.pathname = newpathname;
-				return { redirectUrl: parser.href };
-			}
-		}
+	var ga_map = {
+		"www.google-analytics.com": {from:"/ga.js", to:"/u/ga_debug.js"},
+		"ssl.google-analytics.com": {from:"/ga.js", to:"/u/ga_debug.js"},
+		"stats.g.doubleclick.net": {from:"/dc.js", to:"/dc_debug.js"},
+		"www.google-analytics.com": {from:"/analytics.js", to:"/analytics_debug.js"}
 	}
 
-	if (parser.hostname in domainMods) {
-		currMods = domainMods[parser.hostname];
-		var exploded = explodeQueryString(parser.search);
-		if ("ModPagespeed" in currMods && currMods["ModPagespeed"] != undefined) {
-			exploded["ModPagespeed"] = currMods["ModPagespeed"];
-		}
-		if ("admin" in currMods && currMods["admin"] != undefined) {
-			exploded["admin"] = undefined;
-		}
-		if ("debug" in currMods && currMods["debug"] != undefined) {
-			exploded["debug"] = undefined;
-		}
-		var imploded = implodeQueryString(exploded);
-		if (parser.search != imploded) {
-			parser.search = imploded;
-			return { redirectUrl: parser.href };
-		}
+	var parser = new URL(details.url);
+	if (ga_debug && parser.hostname in ga_map && ga_map[parser.hostname].from == parser.pathname) {
+		parser.pathname = ga_map[parser.hostname].to;
+		console.log('yes');
+		return { redirectUrl: parser.href };
 	}
+if (parser.hostname in ga_map && ga_map[parser.hostname].from == parser.pathname) {
+console.log('no');
+}
+
 }, {urls: ["<all_urls>"]}, ["blocking"]);
 
-chrome.permissions.getAll(function(perm) {
-	// console.log(perm);
-});
+// For domain modifiers (per-domain)
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+	url = new URL(details.url);
+	newRequestHeaders = [];
 
-function updateDomainMods(hostname, mod, value) {
-	if (mod === undefined) {
-		delete domainMods[hostname];
-	} else if (!(hostname in domainMods)) {
-		domainMods[hostname] = {};
-		domainMods[hostname][mod] = value;
-	} else {
-		domainMods[hostname][mod] = value;
+	if (url.hostname in domains) {
+		if ("PageSpeed" in domains[url.hostname]) newRequestHeaders.push({name:"PageSpeed", value:domains[url.hostname]["PageSpeed"]});
+		if ("admin" in domains[url.hostname]) newRequestHeaders.push({name:"Admin", value:domains[url.hostname]["admin"]});
+		if ("debug" in domains[url.hostname]) newRequestHeaders.push({name:"Debug", value:domains[url.hostname]["debug"]});
 	}
-	chrome.storage.local.set({ "domainMods": domainMods });
+	if (newRequestHeaders.length) {
+		return {requestHeaders: details.requestHeaders.concat(newRequestHeaders)};
+	}
+}, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
+
+
+// chrome.permissions.getAll(function(perm) {
+// 	// console.log(perm);
+// });
+
+function updateDomain(hostname, key, value) {
+	if (!(hostname in domains)) {
+		domains[hostname] = {};
+	}
+	if (value !== undefined) {
+		domains[hostname][key] = value;
+	} else if (key in domains[hostname]) { // value === undefined, delete it
+		delete(domains[hostname][key]);
+	}
+	chrome.storage.local.set({domains:domains});
 }
 
-chrome.runtime.onInstalled.addListener(function(details){
-	if(details.reason == "install") {
-		console.log("This is a first install!");
-	} else if(details.reason == "update") {
-		var thisVersion = chrome.runtime.getManifest().version;
-		console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-	}
+chrome.runtime.onInstalled.addListener(function(details) {
+	chrome.storage.local.clear();
+	// if(details.reason == "install") {
+	// 	console.log("This is a first install!");
+	// } else if(details.reason == "update") {
+	// 	var thisVersion = chrome.runtime.getManifest().version;
+	// 	console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+	// }
 });
+
+// function explodeQueryString(qs) {
+// 	var ret = [];
+// 	if (qs != "") {
+// 		var vars = qs.substring(1).split("&");
+// 		for (var i = 0; i < vars.length; i++) {
+// 			var [key, val] = vars[i].split('=', 2);
+// 			key = decodeURIComponent(key);
+// 			val = val === undefined ? undefined : decodeURIComponent(val);
+// 			if (key.substr(-2) == "[]") {
+// 				key = key.substr(0, key.length - 2);
+// 				if (!Array.isArray(ret[key])) ret[key] = [];
+// 				ret[key].push(val);
+// 			} else {
+// 				ret[key] = val;
+// 			}
+// 		}
+// 	}
+// 	// console.log('original qs:',  qs);
+// 	// console.log('exploded qs:',  ret);
+// 	return ret;
+// }
+
+// function implodeQueryString(bits) {
+// 	var ret = Object.entries(bits).map(function(pair) {
+// 		const [key, val] = pair;
+// 		if (Array.isArray(val)) {
+// 			var subret = [];
+// 			val.forEach(function (subval) { subret.push(encodeURIComponent(key + "[]") + "=" + encodeURIComponent(subval)); });
+// 			return subret.join("&");
+// 		} else if (val === undefined) {
+// 			return encodeURIComponent(key);
+// 		} else {
+// 			return encodeURIComponent(key) + "=" + encodeURIComponent(val);
+// 		}
+// 	}).join("&");
+// 	// console.log('imploded qs:',  ret.length ? '?'+ret : '');
+// 	return ret.length ? '?'+ret : '';
+// }
--- popup.js
+++ popup.js
@@ -2,66 +2,69 @@ var background = chrome.extension.getBackgroundPage();
 var currentTab = null;
 var currentTabUrl = null;
 
-var toggle_pagespeed = document.querySelector('[data-key="ModPagespeed"]');
+var toggle_pagespeed = document.querySelector('[data-key="PageSpeed"]');
 var toggle_admin = document.querySelector('[data-key="admin"]');
 var toggle_debug = document.querySelector('[data-key="debug"]');
 var toggle_ga_debug = document.querySelector('[data-key="ga_debug"]');
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	currentTab = tabs[0];
+	currentTabUrl = new URL(currentTab.url.startsWith('view-source:') ? currentTab.url.slice(12) : currentTab.url);
 
-	currentTabUrl = document.createElement("a");
-	currentTabUrl.href = currentTab.url;
+	if (background.ga_debug) toggle_ga_debug.dataset.value = "on";
 
-	if (currentTabUrl.hostname in background.domainMods) {
-		toggle_pagespeed.dataset.value = background.domainMods[currentTabUrl.hostname]["ModPagespeed"];
-		toggle_admin.dataset.value = background.domainMods[currentTabUrl.hostname]["admin"];
-		toggle_debug.dataset.value = background.domainMods[currentTabUrl.hostname]["debug"];
-		toggle_ga_debug.dataset.value = background.domainMods[currentTabUrl.hostname]["ga_debug"];
+	if (currentTabUrl.hostname in background.domains) {
+		if ("PageSpeed" in background.domains[currentTabUrl.hostname]) toggle_pagespeed.dataset.value = background.domains[currentTabUrl.hostname]["PageSpeed"];
+		if ("admin" in background.domains[currentTabUrl.hostname]) toggle_admin.dataset.value = background.domains[currentTabUrl.hostname]["admin"];
+		if ("debug" in background.domains[currentTabUrl.hostname]) toggle_debug.dataset.value = background.domains[currentTabUrl.hostname]["debug"];
 	} else {
 		// authorize tab via optional permissions!!
 	}
 });
 
 function dual_switch_click(e) {
-	this.dataset.value = (this.dataset.value == "undefined") ? "on" : "undefined";
-	background.updateDomainMods(currentTabUrl.hostname, this.dataset.key, this.dataset.value == "undefined" ? undefined : this.dataset.value);
-	// chrome.tabs.reload(currentTab.id, { bypassCache:true });
-
-	var exploded = background.explodeQueryString(currentTabUrl.search);
-	delete exploded[this.dataset.key];
-	currentTabUrl.search = background.implodeQueryString(exploded);
-	chrome.tabs.update(currentTab.id, {url: currentTabUrl.href});
-
+	if (this.dataset.value === undefined) {
+		this.dataset.value = "on";
+	} else {
+		delete(this.dataset.value);
+	}
+	background.updateDomain(currentTabUrl.hostname, this.dataset.key, this.dataset.value);
+	chrome.tabs.reload(currentTab.id, { bypassCache:true });
 }
 
 function tri_switch_click(e) {
-	if (e.offsetX < (this.clientWidth/2)) { // click on the left
-		this.dataset.value = (this.dataset.value == "undefined") ? "off" : "undefined";
-	} else {
-		this.dataset.value = (this.dataset.value == "undefined") ? "on" : "undefined";
+	if (e.offsetX < (this.clientWidth/2) && this.dataset.value === undefined) { // click on the left
+		this.dataset.value = "off";
+	} else if (this.dataset.value === undefined) { // click on the right
+		this.dataset.value = "on";
+	} else { // already set, so unset
+		delete(this.dataset.value);
 	}
-	background.updateDomainMods(currentTabUrl.hostname, this.dataset.key, this.dataset.value == "undefined" ? undefined : this.dataset.value);
-	// chrome.tabs.reload(currentTab.id, { bypassCache:true });
+	background.updateDomain(currentTabUrl.hostname, this.dataset.key, this.dataset.value);
+	chrome.tabs.reload(currentTab.id, { bypassCache:true });
+}
 
-	var exploded = background.explodeQueryString(currentTabUrl.search);
-	delete exploded[this.dataset.key];
-	currentTabUrl.search = background.implodeQueryString(exploded);
-	chrome.tabs.update(currentTab.id, {url: currentTabUrl.href});
-
+function ga_debug_switch_click(e) {
+	if (this.dataset.value === undefined) {
+		this.dataset.value = "on";
+	} else {
+		delete(this.dataset.value);
+	}
+	background.ga_debug = this.dataset.value !== undefined;
+	chrome.tabs.reload(currentTab.id, { bypassCache:true });
 }
 
 toggle_pagespeed.addEventListener("click", tri_switch_click);
 toggle_admin.addEventListener("click", dual_switch_click);
 toggle_debug.addEventListener("click", dual_switch_click);
-toggle_ga_debug.addEventListener("click", dual_switch_click);
+toggle_ga_debug.addEventListener("click", ga_debug_switch_click);
 
 
 // document.querySelector('button#reset').addEventListener('click', function(e) {
-// 	background.updateDomainMods(currentTabUrl.hostname);
+// 	background.updateDomain(currentTabUrl.hostname);
 
 // 	var exploded = background.explodeQueryString(currentTabUrl.search);
-// 	delete exploded["ModPagespeed"];
+// 	delete exploded["PageSpeed"];
 // 	delete exploded["admin"];
 // 	delete exploded["debug"];
 // 	currentTabUrl.search = background.implodeQueryString(exploded);
@@ -70,11 +73,36 @@ toggle_ga_debug.addEventListener("click", dual_switch_click);
 // });
 
 
-// Temp test for oauth
-document.querySelector('button#oauth').addEventListener('click', function() {
-	chrome.identity.getAuthToken({interactive: true}, function(token) {
-		console.log(token);
-	});
-});
+// // Temp test for oauth
+// document.querySelector('button#oauth').addEventListener('click', function() {
+// 	url = new URL('https://auth.compucast.com/authorize');
+// 	url.searchParams.set('response_type', 'code');
+// 	url.searchParams.set('client_id', 'compucast-assistant');
+// 	url.searchParams.set('redirect_uri', chrome.identity.getRedirectURL());
+// 	// url.searchParams.set('scope', 'openid basic');
+// 	url.searchParams.set('state', 'xyz');
+// 	chrome.identity.launchWebAuthFlow({url:url.href, interactive:true}, function(responseUrl) {
+// 		rurl = new URL(responseUrl);
+// 		if (rurl.searchParams.get('state') == url.searchParams.get('state')) {
+// 			authcode = rurl.searchParams.get('code');
+
+// 			url2 = new URL('https://auth.compucast.com/token');
+// 			url2.searchParams.set('grant_type', 'authorization_code');
+// 			url2.searchParams.set('client_id', 'compucast-assistant');
+// 			url2.searchParams.set('client_secret', 'compucast-assistant');
+// 			url2.searchParams.set('redirect_uri', chrome.identity.getRedirectURL());
+// 			url2.searchParams.set('code', authcode);
+
+// 			chrome.identity.launchWebAuthFlow({url:url2.href, interactive:true}, function(responseUrl) {
+// 				return true;
+// 			});
+
+// 		}
+// 	});
+
+// 	// chrome.identity.getAuthToken({interactive: true}, function(token) {
+// 	// 	console.log(token);
+// 	// });
+// });
 
 
